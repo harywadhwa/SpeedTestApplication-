e18c3754933d00ab4c547c7907e17a91e610c7e9
Harsh Wadhwa <hwadhwa050@gmail.com>
2023-12-24 12:04:21 +0530

    Merged files

--- client.py
+++ client.py
@@ -1,33 +1,124 @@
-import speedtest
 import socket
 import time
 
-def get_ping():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('localhost', 5555))
+HOST = 'localhost'  # Server IP address
+PORT = 1234       # Server port number
+BUFFER_SIZE = 8192
+
+def calculate_speed(start_time, end_time, data_size):
+    duration = end_time - start_time
+    speed = data_size / duration  # Bytes per second
+    speed = speed / 125000  # Convert to Mbps
+    return speed
+
+
+
+def download(client_socket):
+    sum_speed = 0.0
+
+    for i in range(10):
+        download_speed = receive_data(client_socket)
+        sum_speed += download_speed
+
+    average_speed = sum_speed / 10
+    print("Average download speed is:", average_speed, "Mbps")
+
+
+def upload(client_socket):
+    sum = 0.0
+    for i in range(0,10):
+        upload_speed = send_data(client_socket)
+        print('Data sent successfully.')
+        print('upload speed is : ',upload_speed,' Mbps')
+        sum += upload_speed
+
+    print("average upload speed is : ",sum/10)
+
+
+
+
+def receive_data(client_socket):
+    data_size = 0
+    expected_data_size = 10 * 1024 * 1024  # 10 MB
+
     start_time = time.time()
-    client_socket.sendall('ping'.encode('utf-8'))
-    client_socket.recv(1024)
+
+    while data_size < expected_data_size:
+        chunk = client_socket.recv(BUFFER_SIZE)
+        if not chunk:
+            break
+        data_size += len(chunk)
+
     end_time = time.time()
+
+    download_speed = calculate_speed(start_time, end_time, data_size)
+    print('Data received successfully.')
+    print('Download speed is:', download_speed, 'Mbps')
+    return download_speed
+
+
+
+def send_data(client_socket):
+
+    data = b'X' * 10 * 1024 * 1024  # 10 MB
+    client_socket.sendall(data)
+
+    upload_speed = b''
+    while True:
+        chunk = client_socket.recv(BUFFER_SIZE)
+        upload_speed += chunk
+        if b'\n' in chunk:
+            break
+
+    return float(upload_speed.decode().strip())
+
+if __name__ == '__main__':
+    client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    client_socket.connect((HOST, PORT))
+    
+    download(client_socket)
+    upload(client_socket)
+
     client_socket.close()
-    ping_time = end_time - start_time
-    return ping_time
 
-def get_speed():
-    st = speedtest.Speedtest()
-    st.get_best_server()  # Automatically selects the best server
-    download_speed = st.download() / 1_000_000  # Convert to Mbps
-    upload_speed = st.upload() / 1_000_000  # Convert to Mbps
-    return download_speed, upload_speed 
+# import socket
+# import time
+
+# HOST = 'localhost'  # Server IP address
+# PORT = 1234          # Server port number
+# BUFFER_SIZE = 8192
+
+# def receive_data(client_socket):
+#     data_size = 0
+#     expected_data_size = 10 * 1024 * 1024  # 10 MB
+
+#     start_time = time.time()
+
+#     while data_size < expected_data_size:
+#         chunk = client_socket.recv(BUFFER_SIZE)
+#         if not chunk:
+#             break
+#         data_size += len(chunk)
+
+#     end_time = time.time()
+
+#     download_speed = calculate_download_speed(start_time, end_time, data_size)
+#     print('Data received successfully.')
+#     print('Download speed is:', download_speed, 'Mbps')
+#     return download_speed
 
 
-def main():
-    ping_time = get_ping()
-    download_speed, upload_speed = get_speed()
 
-    print(f"Ping Time: {ping_time:.2f} seconds")
-    print(f"Download Speed: {download_speed:.2f} Mbps")
-    print(f"Upload Speed: {upload_speed:.2f} Mbps")
+# if __name__ == '__main__':
+#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     client_socket.connect((HOST, PORT))
+#     sum_speed = 0.0
 
-if __name__ == "__main__":
-    main()
+#     for i in range(10):
+#         download_speed = receive_data(client_socket)
+#         sum_speed += download_speed
+
+#     average_speed = sum_speed / 10
+#     print("Average download speed is:", average_speed, "Mbps")
+
+#    
--- server.py
+++ server.py
@@ -1,24 +1,126 @@
 import socket
+import time
 import threading
+HOST = 'localhost'  # Server IP address
+PORT = 1234        # Server port number
+BUFFER_SIZE = 8192  # 8 KB
+FORMAT = "utf-8"
 
-def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('0.0.0.0', 5555))
-    server_socket.listen(5)
+def calculate_speed(start_time, end_time, data_size):
+    duration = end_time - start_time
+    speed = data_size / duration  # Bytes per second
+    speed = speed/125000
+    return speed
 
-    print("Server listening on port 5555...")
+def download(client_socket, client_address):
+    while True:
+        start_time = time.time()
+        data_size = 0
+        expected_data_size = 10 * 1024 * 1024  # 10 MB
+
+        # Simulate sending data from server to client (for testing purposes)
+        simulated_data = b'0' * expected_data_size
+        client_socket.sendall(simulated_data)
+
+        end_time = time.time()
+
+        download_speed = calculate_speed(start_time, end_time, expected_data_size)
+        download_speed_str = str(download_speed) + '\n'
+        print('Download speed:', download_speed, 'Mbps')
+        client_socket.send(download_speed_str.encode(FORMAT))
+
+def upload(client_socket, client_address):
+    while True :
+        start_time = time.time()
+        data_size = 0
+        expected_data_size = 10 * 1024 * 1024  # 10 MB
+
+        while data_size < expected_data_size:
+            data = client_socket.recv(BUFFER_SIZE)
+            if not data:
+                break
+            data_size += len(data)
+
+        end_time = time.time()
+
+        if data_size == 0:
+            print("connection with ",client_address," is closed")
+            client_socket.close()
+            break
+
+        upload_speed = calculate_speed(start_time, end_time, data_size)
+        upload_speed_str = str(upload_speed) + '\n'
+        print('Upload speed:', upload_speed, 'Mbps')
+        client_socket.send(upload_speed_str.encode(FORMAT))
+
+def start_server(client_socket,client_address):
+    upload(client_socket, client_address)
+    
+
+def listen():
+
+    server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server_socket.bind((HOST,PORT))
 
     while True:
-        client_socket, addr = server_socket.accept()
-        print(f"Accepted connection from {addr}")
-        threading.Thread(target=handle_client, args=(client_socket,)).start()
+        print('Server listening on {}:{}'.format(HOST, PORT))
+        server_socket.listen(3)
+        client_socket, client_address = server_socket.accept()
+        print("connected to ",client_address)
+        thread = threading.Thread(target=start_server, args=(client_socket, client_address))
+        thread.start()
 
-def handle_client(client_socket):
-    data = client_socket.recv(1024).decode('utf-8')
-    if data == 'ping':
-        client_socket.sendall('pong'.encode('utf-8'))
-        print("data sent to", client_socket)
-    client_socket.close()
 
-if __name__ == "__main__":
-    start_server()
+if __name__ == '__main__':
+    listen()
+
+
+# import socket
+# import time
+# import threading
+
+# HOST = ''  # Server IP address
+# PORT = 1234  # Server port number
+# BUFFER_SIZE = 8192  # 8 KB
+# FORMAT = "utf-8"
+
+# def calculate_download_speed(start_time, end_time, data_size):
+#     duration = end_time - start_time
+#     speed = data_size / duration  # Bytes per second
+#     speed = speed / 125000  # Convert to Mbps
+#     return speed
+
+# def start_server(client_socket, client_address):
+#     while True:
+#         start_time = time.time()
+#         data_size = 0
+#         expected_data_size = 10 * 1024 * 1024  # 10 MB
+
+#         # Simulate sending data from server to client (for testing purposes)
+#         simulated_data = b'0' * expected_data_size
+#         client_socket.sendall(simulated_data)
+
+#         end_time = time.time()
+
+#         upload_speed = calculate_download_speed(start_time, end_time, expected_data_size)
+#         upload_speed_str = str(upload_speed) + '\n'
+#         print('Download speed:', upload_speed, 'Mbps')
+#         client_socket.send(upload_speed_str.encode(FORMAT))
+
+# def listen():
+#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server_socket.bind((HOST, PORT))
+
+#     while True:
+#         print('Server listening on {}:{}'.format(HOST, PORT))
+#         server_socket.listen(3)
+#         client_socket, client_address = server_socket.accept()
+#         print("Connected to ", client_address)
+#         thread = threading.Thread(target=start_server, args=(client_socket, client_address))
+#         thread.start()
+
+# if __name__ == '__main__':
+#     listen()
+
+
+
